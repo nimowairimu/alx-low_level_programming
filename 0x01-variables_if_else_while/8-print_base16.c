6e8192a827cdbac9fe40be978b0e7d4a74cdec7d
nimowairimu <nimowairimu25@gmail.com>
2022-07-01 03:58:43 -0700

    Add changes

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- *main - Prints all the numbers of base 16 in lowercase.
+ * main - Prints all the numbers of base 16 in lowercase.
  *
- *Return: Always 0.
- *
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -12,9 +11,10 @@ int main(void)
 
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
+	
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
-        
+	
 	putchar('\n');
 
 	return (0);
