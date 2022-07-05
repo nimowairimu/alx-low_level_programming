591786c58bb6c41835697f694a56d6606a67d76a
nimowairimu <nimowairimu25@gmail.com>
2022-07-05 03:52:44 -0700

    Print character

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main prints putchar followed by a new line 
+ *
+ * Return is always 0
+ */
+int main (void)
+{ 
+	_putcar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+        
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Learning nested loos in C
