18188a69ead214723fad62331a64cefca9463aab
nimowairimu <nimowairimu25@gmail.com>
2022-07-05 12:24:49 -0700

    Correctv

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,9 +12,10 @@ void times_table(void)
 		_putchar('0');
 
 		for (mult = 1; mult <= 9; mult++)
-                {
-			_putchar(',');																		_putchar(' ');
-	
+		
+			_putchar(',');
+		        _putchar(' ');
+
 			prod = num * mult;
 
 			if (prod <= 9)
