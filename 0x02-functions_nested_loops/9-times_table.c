5e779f801a9884b8a92c533bc283d6fcc761f05b
nimowairimu <nimowairimu25@gmail.com>
2022-07-05 12:20:05 -0700

    Correctv

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,8 @@ void times_table(void)
 		                 _putchar(' ');
 	
 			else
-				_putchar((prod / 10) + '0');															_putchar((prod % 10) + '0');
+				_putchar((prod / 10) + '0');															
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n')
 	}
