5f3e022fdd4b97ae9d3937a9fda25ca6970800b4
nimowairimu <nimowairimu25@gmail.com>
2022-07-05 04:43:55 -0700

    Remove spaces

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-* main prints putchar followed by a new line 
-* Return is always 0
+* main - Prints "_putchar " followed by a new line 
+* Return: Always 0.
 */
 int main (void)
 { 
@@ -15,5 +15,5 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 
-return 0;
+return (0);
 }
