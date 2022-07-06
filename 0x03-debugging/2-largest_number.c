3d1f1e170be25b17c6d81fed128235ef3af3281e
nimowairimu <nimowairimu25@gmail.com>
2022-07-06 06:15:42 -0700

    Check case

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,12 +5,12 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
- *
  * Return: largest number
  */
 int largest_number(int a, int b, int c)
 {
 	int largest;
+
 	if (a > b && b > c)
 		largest = a;
 	else if (a > b && b < c)
