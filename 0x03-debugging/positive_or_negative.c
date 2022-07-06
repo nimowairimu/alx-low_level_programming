12ebb3d50cd7b2fc61fcca1ea2713bbb92f60b8e
nimowairimu <nimowairimu25@gmail.com>
2022-07-06 08:58:18 -0700

    Add correct

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -7,9 +7,11 @@
  * and prints the last digit of the number stored in the variable n 
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
