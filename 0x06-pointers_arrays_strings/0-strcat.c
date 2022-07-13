3e5630d44df6b47cbd7f5829a17a3509debe9c8b
nimowairimu <nimowairimu25@gmail.com>
2022-07-13 12:11:53 -0700

    Joins string s

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * strcat - Concatenates the string pointed to by @src, including the terminatig
- *           null byte, to the end of the string pointed to by @dest.
+ * strcat - Concatenates the string pointed including the terminatig
+ * null byte,the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
  *
@@ -11,10 +11,10 @@
 char *strcat(char *dest, const char *src)
 {
 	int index = 0, dest_len = 0;
-	
+
 	while (dest[index++])
 		dest_len++;
-	
+
 	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
 
