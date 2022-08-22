ea34f7332cbc877350b00e50c1c61ca29f5f1d0b
nimowairimu <nimowairimu25@gmail.com>
2022-08-22 07:13:19 -0700

    Save changes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -87,7 +87,7 @@ int main(int argc, char *argv[])
 		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
 	} while (r > 0);
-	
+
 	free(buffer);
 	close_file(from);
 	close_file(to);
